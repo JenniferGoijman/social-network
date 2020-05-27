a1e42e34fed4400deea3a0aae6857c3ec05349d0
JenniferGoijman <jgoijman@gmail.com>
2020-05-27 14:44:21 +0200

    add Noah font

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,9 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 import App from './App.jsx';
-import WebFont from 'webfontloader';
-
-
-WebFont.load({
-  google: {
-    families: ['Noah Regular', 'sans-serif']
-  }
-});
+import './fonts/noah-regular.otf'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
