7deec34449d4c576a7ef3972dca672296927ac4f
JenniferGoijman <jgoijman@gmail.com>
2020-06-01 16:25:15 +0200

    setting container

--- frontend/src/containers/User/Settings/Settings.jsx
+++ frontend/src/containers/User/Settings/Settings.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { IMAGES_URL } from '../../../api-config';
 import './Settings.scss'
-import { Input, Button } from 'antd';
+import { Form, Input, Button } from 'antd';
 
 const Settings = props => {
+    //const [form] = Form.useForm();
     const { TextArea } = Input;
+    const [myUser, setMyUser] = useState(props.myUser);
 
-    return (
+    const onFinish = values => {
+        console.log(values);
+    }
+    
+    return (        
         <div className="settings-container">
             <div className="settings-items-container">
                 <div className="settings-item-name">
@@ -15,40 +21,55 @@ const Settings = props => {
                         <img src={IMAGES_URL + props.myUser.pic} alt="Foto de perfil" />
                     </div>
                     <div className="name">
-                        <h2>{props.myUser.username}</h2>
+                        <h2>{myUser.username}</h2>
                         <h4><a href="#">Cambiar foto de perfil</a></h4>
                     </div>
                 </div>
-                <div className="settings-item">
-                    <div className="left">Nombre</div>
-                    <Input placeholder="Nombre" />
-                </div>
-                <div className="settings-item">
-                    <div className="left">Nombre de usuario</div>
-                    <Input placeholder="Nombre de usuario" />
-                </div>
-                <div className="settings-item">
+                <Form name="settings" onFinish={onFinish} initialValues={{myUser}}>
+                    <div className="settings-item">
+                        <div className="left">Nombre</div>
+                        <Form.Item name={['myUser', 'name']}>
+                            <Input name="name" placeholder="Nombre" style={{ width: 300 }}/>
+                        </Form.Item>
+                    </div>
+                    <div className="settings-item">
+                        <div className="left">Nombre de usuario</div>
+                        <Form.Item name={['myUser', 'username']}>
+                            <Input name="username" placeholder="Nombre" style={{ width: 300 }}/>
+                        </Form.Item>
+                    </div>
+                    <div className="settings-item">
+                        <div className="left">Biografía</div>
+                        <Form.Item name={['myUser', 'description']}>
+                            <TextArea name="description" autoSize={{ minRows: 2, maxRows: 4 }} style={{ width: 300 }}/>
+                        </Form.Item>
+                    </div>
+                    <div className="settings-item">
+                        <div className="left">Correo electrónico</div>
+                        <Form.Item name={['myUser', 'email']}>
+                        <Input name="email" placeholder="Correo electrónico" style={{ width: 300 }}/>
+                        </Form.Item>
+                    </div>
+                    <div className="settings-item">
+                    <div className="left"></div>
+                        <div className="name">
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit">
+                                    Aceptar
+                                </Button>
+                            </Form.Item>
+                        </div>
+                    </div>
+                </Form>
+
+                {/* <div className="settings-item">
                     <div className="left">Sitio web</div>
                     <Input placeholder="Sitio web" />
-                </div>
-                <div className="settings-item">
-                    <div className="left">Biografía</div>
-                    <TextArea rows={4} />
-                </div>
-                <div className="settings-item">
-                    <div className="left">Correo electrónico</div>
-                    <Input placeholder="Correo electrónico" />
-                </div>
-                <div className="settings-item">
+                </div> */}
+                {/* <div className="settings-item">
                     <div className="left">Número de teléfono</div>
                     <Input placeholder="Número de teléfono" />
-                </div>
-                <div className="settings-item">
-                    <div className="left"></div>
-                    <div className="name">
-                        <Button type="primary" htmlType="submit">Enviar</Button>
-                    </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
